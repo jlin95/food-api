59d72e28ac67c739a0ddb2f4822fce812e7b880d
jlin95 <leeway95@gmail.com>
2018-03-28 15:42:20 +0800

    Update graphiql path in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphiql"
+
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphiql"
   end
+
 
 
   post "/graphql", to: "graphql#execute"
