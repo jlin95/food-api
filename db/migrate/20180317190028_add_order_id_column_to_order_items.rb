024b5d33e61b1a3cda17919066fccc246a3cd283
jlin95 <leeway95@gmail.com>
2018-03-31 18:34:30 +0800

    Update migration

--- db/migrate/20180317190028_add_order_id_column_to_order_items.rb
+++ db/migrate/20180317190028_add_order_id_column_to_order_items.rb
@@ -1,5 +1,5 @@
 class AddOrderIdColumnToOrderItems < ActiveRecord::Migration[5.1]
   def change
-    add_reference :order_items, :order, foreign_key: true
+    # add_reference :order_items, :order, foreign_key: true
   end
 end
