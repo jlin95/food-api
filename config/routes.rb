99230f21ab115a94d4b1f3b783d8ffc3648ea49d
jlin95 <leeway95@gmail.com>
2018-04-08 21:58:15 +0800

    Experimenting with production env details

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphiql"
+  # mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphiql"
 
-  # if Rails.env.development?
-  #   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphiql"
-  # end
+  if Rails.env.production?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+
+  post "/graphql", to: "graphql#execute"
 
 
 
